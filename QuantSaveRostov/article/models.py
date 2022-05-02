e00a2fee148befb0a36b8dc4470ae5731e89526c
vasia sherba <shherbavasja@rambler.ru>
2022-05-02 18:01:01 +0300

    Исправлена модель

--- QuantSaveRostov/article/models.py
+++ QuantSaveRostov/article/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
 # Create your models here.
-User 
-class Article(Model):
-    
-    title = models.
-    prev_img=
-    Pod_title
-    Author
-    data
-    COntent
+
+class Category(models.Model):
+    name = models.CharField(max_length=50,default="Категория")
+ 
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=50,default="Название")
+    prev_img = models.ImageField(upload_to='articles_img')
+    subtitle = models.CharField(max_length=50,default="Под заголовок")
+    authors = models.CharField(max_length=50,default="Участники создания статьи")
+    edit_data = models.DateTimeField(auto_now=True)
+    pub_data = models.DateTimeField('date published')
+    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default=1)
+    content = models.TextField(default="Текст статьи")
+
+    def __str__(self):
+        return self.title
