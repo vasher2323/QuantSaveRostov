08eace3014b9adec1bf73f33b3adc5f80d3d6bf7
vasia sherba <shherbavasja@rambler.ru>
2022-05-04 12:50:03 +0300

    Добавлен скелетон и флаг публикации

--- QuantSaveRostov/article/admin.py
+++ QuantSaveRostov/article/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from article.models import *
 # Register your models here.
+
+
+admin.site.register(Category)
+admin.site.register(Article)
--- QuantSaveRostov/article/models.py
+++ QuantSaveRostov/article/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
@@ -11,12 +12,12 @@ class Category(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length=50,default="Название")
     prev_img = models.ImageField(upload_to='articles_img')
-    subtitle = models.CharField(max_length=50,default="Под заголовок")
+    subtitle = models.CharField(max_length=50,default="Подзаголовок")
     authors = models.CharField(max_length=50,default="Участники создания статьи")
     edit_data = models.DateTimeField(auto_now=True)
     pub_data = models.DateTimeField('date published')
     category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default=1)
-    content = models.TextField(default="Текст статьи")
-
+    content = RichTextField(default="Текст статьи")
+    publish = models.BooleanField(default = False)
     def __str__(self):
         return self.title
