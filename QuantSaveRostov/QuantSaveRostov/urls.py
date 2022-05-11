20d9e6fa8f683f2e06584447210b5ab3cddd39a4
vasia sherba <shherbavasja@rambler.ru>
2022-05-11 11:55:23 +0300

    Добавил шаблон главной и css

--- QuantSaveRostov/QuantSaveRostov/urls.py
+++ QuantSaveRostov/QuantSaveRostov/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from article.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('article/<int:article_id>/', article),
+    path('', index),
 ]
+    
+    
--- QuantSaveRostov/article/views.py
+++ QuantSaveRostov/article/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {'articles': articles}
+    return render(request, 'article/index.html', context)
+
+def article(request, article_id):
+    article = Article.objects.get(PK=article_id)
+    context = {'article': article}
+    return render(request, 'article/article.html', context)
